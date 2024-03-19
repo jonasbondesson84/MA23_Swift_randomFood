8b656c60c513a2d9adb2d44dc49a90b8675efe06
Jonas Bondesson <jonasbondesson@yahoo.com>
2024-03-19 13:45:27 +0100

    first commit

---
+++ randomFood/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  randomFood
+//
+//  Created by Jonas Bondesson on 2024-03-19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
